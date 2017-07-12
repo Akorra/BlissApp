2bea77301b1c14168040bf6421c75edcd7e60a6d
Akorra <neverfearphilishere@gmail.com>
2017-07-12 16:53:37 +0100

    Network Connectivity untested

---
+++ app/src/main/java/recruitment/blissapp/NetworkReceiver.java
@@ -0,0 +1,35 @@
+package recruitment.blissapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by never on 12/07/2017.
+ */
+
+public class NetworkReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        Log.d("RECEIVE", "HEEEEEYOOOOOH");
+        final ConnectivityManager connMgr = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        final android.net.NetworkInfo wifi = connMgr
+                .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        final android.net.NetworkInfo mobile = connMgr
+                .getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if (wifi.isAvailable() || mobile.isAvailable()) {
+            // Do something
+
+            Log.d("Network Available ", "Flag No 1");
+        }
+    }
+}
